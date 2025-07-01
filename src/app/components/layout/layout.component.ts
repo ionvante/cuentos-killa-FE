e5687d33172c9cf7b9cb9f0659f3c688aedd2e4c
ionvante <camusper@gmail.com>
2025-07-01 17:33:08 -0500

    fix layout component ngif

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';  // 🔥 importa esto
+import { Router, RouterOutlet } from '@angular/router';  // 🔥 importa esto
 import { NavbarComponent } from '../navbar/navbar.component';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgIf],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
+  constructor(private router: Router) {}
 
+  get esRutaAdmin(): boolean {
+    return this.router.url.startsWith('/admin');
+  }
 }
