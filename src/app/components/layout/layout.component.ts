58dd685f38462e2bf9fa6aad74589baedac90b9e
ionvante <camusper@gmail.com>
2025-07-02 10:12:26 -0500

    feat: add mobile drawer menu

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { DrawerMenuComponent } from '../drawer-menu/drawer-menu.component';
+import { DrawerService } from '../../services/drawer.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, DrawerMenuComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  constructor(public drawer: DrawerService) {}
+}
