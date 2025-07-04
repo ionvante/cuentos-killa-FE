ca9f597c86f1f794d6be1fdae247b58f2220cc51
ionvante <camusper@gmail.com>
2025-07-04 16:59:20 -0500

    Use stable Sentry packages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api'
+  apiBaseUrl: 'http://localhost:8080/api',
+  sentryDsn: 'https://9c7ad635f6cf04f87a9724db8efeaf9f@o4509612268912640.ingest.us.sentry.io/4509612284116992'
 };
