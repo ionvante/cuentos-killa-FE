a06b0706b143832e720f7b674d72e634b6b6ebbc
ionvante <camusper@gmail.com>
2025-07-04 16:21:18 -0500

    Integrate Sentry for error monitoring

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://cuentos-killa-be-1.onrender.com/api'
+  apiBaseUrl: 'https://cuentos-killa-be-1.onrender.com/api',
+  sentryDsn: 'https://9c7ad635f6cf04f87a9724db8efeaf9f@o4509612268912640.ingest.us.sentry.io/4509612284116992'
 };
