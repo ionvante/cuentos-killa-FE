5c814f7c8cb6889bedce984e73cc5d1178de4335
ionvante <camusper@gmail.com>
2025-07-05 10:20:51 -0500

    Actualizar environment-local.ts

--- src/environments/environment-local.ts
+++ src/environments/environment-local.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api',
-  sentryDsn: 'https://bd1fbbf709cf13f839ce491feec0a1e9@o4509612268912640.ingest.us.sentry.io/4509616124002304'};
+  sentryDsn: 'https://bd1fbbf709cf13f839ce491feec0a1e9@o4509612268912640.ingest.us.sentry.io/4509616124002304',
+/** Monto mínimo para obtener envío gratuito */
+  minFreeShipping: 50
+};
