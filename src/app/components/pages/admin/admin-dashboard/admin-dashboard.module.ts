d2eb4233601a45e1e38299282f7e80754e3e7de2
ionvante <camusper@gmail.com>
2025-07-05 07:00:20 -0500

    fix admin dashboard routing

--- src/app/components/pages/admin/admin-dashboard/admin-dashboard.module.ts
+++ src/app/components/pages/admin/admin-dashboard/admin-dashboard.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { NgChartsModule } from 'ng2-charts';
 import { AdminDashboardComponent } from './admin-dashboard.component';
 import { StatCardComponent } from '../../../stat-card/stat-card.component';
 
+const routes: Routes = [
+  { path: '', component: AdminDashboardComponent }
+];
+
 @NgModule({
   declarations: [AdminDashboardComponent],
-  imports: [CommonModule, FormsModule, NgChartsModule, StatCardComponent],
+  imports: [CommonModule, FormsModule, NgChartsModule, RouterModule.forChild(routes), StatCardComponent],
   exports: [AdminDashboardComponent]
 })
 export class AdminDashboardModule {}
