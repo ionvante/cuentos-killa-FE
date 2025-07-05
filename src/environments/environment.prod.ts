fa43db64f1852913b8a2941d7b7c06035da87ab0
ionvante <camusper@gmail.com>
2025-07-05 10:20:23 -0500

    Actualizar environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://cuentos-killa-be-1.onrender.com/api',
-  sentryDsn: 'https://bd1fbbf709cf13f839ce491feec0a1e9@o4509612268912640.ingest.us.sentry.io/4509616124002304'
+  sentryDsn: 'https://bd1fbbf709cf13f839ce491feec0a1e9@o4509612268912640.ingest.us.sentry.io/4509616124002304',
   /** Monto mínimo para obtener envío gratuito */
   minFreeShipping: 50
 };
