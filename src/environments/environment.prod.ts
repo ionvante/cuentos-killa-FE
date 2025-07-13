ca58634ceee152b084d60dd70e65f54b86e4c28f
ionvante <camusper@gmail.com>
2025-07-13 06:36:42 -0500

    comit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://cuentos-killa-be-1.onrender.com/api',
+  // apiBaseUrl: 'https://cuentos-killa-be-1.onrender.com/api',
+  apiBaseUrl: 'https://cuentos-killa-backend-601022419386.northamerica-south1.run.app/api',
   sentryDsn: 'https://bd1fbbf709cf13f839ce491feec0a1e9@o4509612268912640.ingest.us.sentry.io/4509616124002304',
   /** Monto mínimo para obtener envío gratuito */
   minFreeShipping: 50
