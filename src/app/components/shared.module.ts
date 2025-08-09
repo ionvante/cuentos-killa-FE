b56a7e8a4a95edaa8209aea178e16e71a8f8ffa0
ionvante <camusper@gmail.com>
2025-08-09 00:03:38 -0500

    refactor: simplify shared module

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -1,19 +1,13 @@
-import { NgModule ,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CuentoCardComponent } from './cuento-card/cuento-card.component';
 import { LazyLoadImageDirective } from '../directives/lazy-load-image.directive';
 import { FlyToCartDirective } from '../directives/fly-to-cart.directive';
-// import { CuentosGridComponent } from './cuentos-grid/cuentos-grid.component';
-
-const routes: Routes = [
-  { path: '', component: CuentoCardComponent }
-];
 
 @NgModule({
   declarations: [CuentoCardComponent],
   imports: [CommonModule, LazyLoadImageDirective, FlyToCartDirective],
-  exports: [CuentoCardComponent, LazyLoadImageDirective, FlyToCartDirective], // 🔥 esto es clave
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  exports: [CuentoCardComponent, LazyLoadImageDirective, FlyToCartDirective]
 })
 export class SharedModule {}
+
