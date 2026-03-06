7cc9606c4732f84268efa90b5159b4abf7fcc16e
ionvante <camusper@gmail.com>
2026-03-06 06:51:18 -0500

    chore(config): Update API base URL to new Railway production backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   // apiBaseUrl: 'https://cuentos-killa-be-1.onrender.com/api',
-  apiBaseUrl: 'https://cuentos-killa-backend-601022419386.northamerica-south1.run.app/api/v1',
+  // apiBaseUrl: 'https://cuentos-killa-backend-601022419386.northamerica-south1.run.app/api/v1',
+  apiBaseUrl: 'https://cuentos-killa-be-production.up.railway.app/api/v1',
   /** Monto mínimo para obtener envío gratuito */
   minFreeShipping: 50
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api/v1',
+  // apiBaseUrl: 'http://localhost:8080/api/v1',
+  apiBaseUrl: 'https://cuentos-killa-be-production.up.railway.app/api/v1',
   /** Monto mínimo para obtener envío gratuito */
   minFreeShipping: 50
 };
